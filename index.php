00c02a856ff58ae8279d407e6286ab243021a06d
Axel <axelmol2018@gmail.com>
2025-06-30 15:45:11 -0500

    button

--- index.php
+++ index.php
@@ -563,7 +563,7 @@
                         </p>
 
                         <br>
-                        <button href="./somos.php" class="button">
+                        <button onclick="window.location.href='./somos.php'" class="button">
                             <span class="button__icon-wrapper">
                                 <svg viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="button__icon-svg" width="10">
                                     <path d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z" fill="currentColor"></path>
